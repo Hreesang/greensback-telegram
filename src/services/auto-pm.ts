8920edd71a5fa7cdfb4f5a97c0cbfbd23bb7425b
Hreesang <officialhreesang@gmail.com>
2022-08-14 17:53:23 +0700

    add caching to get unseen users

--- src/services/auto-pm.ts
+++ src/services/auto-pm.ts
@@ -1,31 +1,47 @@
 import { NewMessageEvent } from 'telegram/events';
 import keywords from '@/config/auto-pm-keywords.json';
 import { getDisplayName } from 'telegram/Utils';
+import { Api } from 'telegram';
 
 export const processAutoPM = async (event: NewMessageEvent) => {
   const client = event.client;
   const messageEvent = event.message;
-  const sender = await messageEvent.getSender().catch((e) => console.log(e));
 
-  if (client && messageEvent.isGroup && sender) {
-    const senderName = getDisplayName(sender);
-    let sentPMs = 0;
+  try {
+    const group = await messageEvent.getChat();
 
-    for (const [keyword, message] of Object.entries(keywords)) {
-      if (messageEvent.message.match(keyword)) {
-        try {
+    if (client && group) {
+      await client.getParticipants(group);
+
+      const sender = (await messageEvent.getSender()) as Api.User;
+      let sentPMs = 0;
+
+      for (const [keyword, message] of Object.entries(keywords)) {
+        if (messageEvent.message.match(keyword)) {
           await client.sendMessage(sender, { message });
           sentPMs++;
-        } catch (e) {
-          console.log(e);
+        }
+      }
+
+      if (sentPMs > 0) {
+        let senderName: string;
+
+        if (sender.username) {
+          senderName = '@' + sender.username;
+        } else if (sender.phone) {
+          senderName = `${getDisplayName(sender)} (+${sender.phone})`;
+        } else {
+          senderName = getDisplayName(sender);
+        }
+
+        if (sentPMs === 1) {
+          console.log(`An auto PM has been sent to ${senderName}.`);
+        } else {
+          console.log(`${sentPMs} auto PMs have been sent to ${senderName}.`);
         }
       }
     }
-
-    if (sentPMs === 1) {
-      console.log(`An auto PM has been sent to @${senderName}.`);
-    } else if (sentPMs > 1) {
-      console.log(`${sentPMs} auto PMs have been sent to @${senderName}.`);
-    }
+  } catch (e) {
+    console.log(e);
   }
 };
