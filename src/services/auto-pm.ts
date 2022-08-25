ac47c75396c75c46ec864e600a4fe353c02b6adb
Hreesang <officialhreesang@gmail.com>
2022-08-25 10:19:57 +0700

    fix performance issue in getting sender entity

--- src/services/auto-pm.ts
+++ src/services/auto-pm.ts
@@ -17,37 +17,6 @@ class AutoPM {
     return undefined;
   };
 
-  private getSender = async (message: Api.Message, chat: Entity) => {
-    try {
-      let sender = (
-        message.sender ? message.sender : await message.getSender()
-      ) as Api.User;
-
-      if (!sender.username && !sender.phone) {
-        console.log("sender doesn't have username and phone. getting it...");
-
-        const client = message.client as TelegramClient;
-        await client.getDialogs();
-
-        const participants = await client.getParticipants(chat);
-        for (const participant of participants) {
-          if (participant.id === sender.id) {
-            sender = participant;
-            break;
-          }
-        }
-
-        console.log(
-          `done! username is ${sender.username}, and phone number is ${sender.phone}.`
-        );
-      }
-
-      return sender;
-    } catch (e) {
-      throw e;
-    }
-  };
-
   private getSenderName = (sender: Api.User) => {
     let senderName: string;
 
@@ -64,29 +33,36 @@ class AutoPM {
 
   private sendMessage = async (
     client: TelegramClient,
+    chat: Api.Chat,
     sender: Api.User,
     text: string
   ) => {
     try {
-      const inputSender = await client.getInputEntity(
-        sender.username ?? sender.id
-      );
-      await client.sendMessage(inputSender, { message: text });
+      await client.sendMessage(sender.id, { message: text });
     } catch (e: any) {
-      const senderName = this.getSenderName(sender);
-      const errMessage = e.any ?? '';
-
-      throw new Error(
-        `Can't send a direct message to sender: ${senderName}.\n${errMessage}`
-      );
+      try {
+        await client.getParticipants(chat);
+        await client.sendMessage(sender.id, { message: text });
+      } catch (e: any) {
+        if (e.message) {
+          const senderName = this.getSenderName(sender);
+          throw new Error(
+            `Can't send a direct message to sender: ${senderName}.\n${e.message}`
+          );
+        } else {
+          throw e;
+        }
+      }
     }
-    return sender;
   };
 
   public onEvent = async (event: NewMessageEvent) => {
     const message = event.message;
-    const text = message.message;
+    if (!message.isGroup) {
+      return;
+    }
 
+    const text = message.message;
     const keyword = this.hasKeyword(text);
     if (!keyword) {
       return;
@@ -94,17 +70,17 @@ class AutoPM {
     console.log('passed keyword check');
 
     try {
-      const chat = message.chat ? message.chat : await message.getChat();
+      const chat = (message.chat ? message.chat : await message.getChat()) as
+        | Api.Chat
+        | undefined;
       if (!chat) {
-        if (message.isGroup) {
-          throw new Error("Can't find the group message chat.");
-        }
-
-        return;
+        throw new Error("Can't find the chat entity.");
       }
       console.log('passed group check');
 
-      let sender = await this.getSender(message, chat);
+      const sender = (
+        message.sender ? message.sender : await message.getSender()
+      ) as Api.User | undefined;
       if (!sender) {
         throw new Error("Can't get the message sender.");
       }
@@ -116,7 +92,7 @@ class AutoPM {
       }
       console.log('passed client check');
 
-      sender = await this.sendMessage(client, sender, keyword.text);
+      await this.sendMessage(client, chat, sender, keyword.text);
 
       const senderName = this.getSenderName(sender);
       console.log(`An auto PM (${keyword.key}) has sent to ${senderName}.`);
